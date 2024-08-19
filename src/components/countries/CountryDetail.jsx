0c66444cc0524f024fc49d248bfaa8348560b3c6
Abdul-Qayyum Olatunji <106759130+Qayyax@users.noreply.github.com>
2024-08-19 13:05:59 -0400

    finished style for contry detail page

--- src/components/countries/CountryDetail.jsx
+++ src/components/countries/CountryDetail.jsx
@@ -13,7 +13,7 @@ export default function CountryDetail() {
   const { isDarkMode } = useContext(DarkModeContext);
 
   const textColor = isDarkMode ? '#FFFFFF' : '#111517';
-  const bgColor = isDarkMode ? '#202C36' : '#FFFFFF';
+  const bgColor = isDarkMode ? '#2B3844' : '#FFFFFF';
   if (!data) return (
     <h1 className="flex items-center justify-center m-auto">No Country to display</h1>
   )
@@ -37,7 +37,10 @@ export default function CountryDetail() {
     // to={`/countries/${pageID}/${countryData.name}`}
     return (
       <button
-        className="p-4 border border-black"
+        className="min-w-[96px] py-1.5 px-4 rounded-md shadow-md font-light"
+        style={{
+          background: bgColor
+        }}
         key={index}
       >
         {countryData.name}
@@ -49,9 +52,12 @@ export default function CountryDetail() {
   // {formatText("Languages", data.languages)}
 
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center lg:items-start lg:px-20">
       <div
-        className="flex flex-col pt-10 gap-16 items-start"
+        className="flex flex-col pt-10 pb-16 gap-16 items-start justify-start lg:gap-20 lg:justify-between w-full"
+        style={{
+          color: textColor
+        }}
       >
         <button
           onClick={() => navigate(-1)}
@@ -64,30 +70,54 @@ export default function CountryDetail() {
           <span><MdOutlineKeyboardBackspace /></span>
           Back
         </button>
-        <div>
+        <div
+          className="flex flex-col gap-y-11 lg:flex-row lg:justify-between 
+           lg:gap-x-36 w-full lg:items-center"
+        >
           <img
             src={data.flags.png}
             alt={data.flags.alt}
+            className="rounded-md w-full max-w-[320px] max-h-[229px] shadow-md lg:max-w-[560px] lg:max-h-[401px]"
           />
-          <div>
-            <h1>{data.name}</h1>
-            <div>
-              <div>
+          <div
+            className="w-full"
+          >
+            <h1
+              className="font-extrabold mb-4"
+            >
+              {data.name}
+            </h1>
+            <div
+              className="flex flex-col gap-y-8 mb-8 lg:flex-row lg:gap-x-28"
+            >
+              <div
+                className="flex flex-col gap-y-2 w-full"
+              >
                 {formatText("Native Name", data.nativeName)}
                 {formatText("Population", new Intl.NumberFormat("en-US").format(data.population))}
                 {formatText("Region", data.region)}
                 {formatText("Sub Region", data.subRegion)}
                 {formatText("Capital", data.capital)}
               </div>
-              <div>
+              <div
+                className="flex flex-col gap-y-2 w-full"
+              >
                 {formatText("Top Level Domain", data.tld)}
                 {formatText("Currencies", data.currency)}
                 {formatText("Languages", data.languages)}
               </div>
             </div>
-            <div>
-              <h2>Border Countries:</h2>
-              <div>
+            <div
+              className="flex flex-col gap-y-4 lg:flex-row lg:items-center lg:gap-x-4"
+            >
+              <h2
+                className="font-bold"
+              >
+                Border Countries:
+              </h2>
+              <div
+                className="flex gap-x-2.5"
+              >
                 {/*Function that shows the button*/
                   // change border countries in search data to fetch the countryies ahead of time.
                   borders.map((item, index) => {
